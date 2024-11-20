dd5faf5d73c54f5e9b31f339f4026fff1cd8bd1a
harshrawate <harshrawate7@gmail.com>
2024-11-20 19:13:17 +0530

    minor change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
                    I'm a Full Stack Developer
                </h2>
                <p className='text-gray-500 py-4 max-w-md'>
-               I’m driven by a strong enthusiasm for solving complex problems, learning new technologies, and delivering impactful software solutions.
+               I’m driven by aa strong enthusiasm for solving complex problems, learning new technologies, and delivering impactful software solutions.
                </p>
                <div>
                 <Link to="portfolio" smooth duration={500} className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer '>
